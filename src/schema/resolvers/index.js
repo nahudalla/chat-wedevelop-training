3d78d2cd5259494a8cfd1431360beb7f202c3585
Nahuel Dallacamina <nahudalla@gmail.com>
2019-05-06 16:52:31 -0300

    Fixed code style for resolvers loader

--- src/schema/resolvers/index.js
+++ src/schema/resolvers/index.js
@@ -1,15 +1,10 @@
 import path from 'path'
 import fs from 'fs'
 
-const resolvers = {}
-
-fs
+const resolvers = fs
   .readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
-  .forEach(file => {
-    const resolverPath = path.join(__dirname, file)
-    const resolver = require(resolverPath)
-    Object.assign(resolvers, resolver)
-  })
+  .map(file => path.join(__dirname, file))
+  .reduce((acc, resolverPath) => ({ ...acc, ...require(resolverPath) }), {})
 
 export default resolvers
