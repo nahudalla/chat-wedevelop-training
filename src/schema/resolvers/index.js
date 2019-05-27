964e9316777d561defc40bfdbb71c818d62777a3
Nahuel Dallacamina <nahudalla@gmail.com>
2019-05-27 08:33:09 -0300

    Fixed bug when loading resolvers, where only one Query and one Mutation were loaded.

--- src/schema/resolvers/index.js
+++ src/schema/resolvers/index.js
@@ -1,10 +1,11 @@
 import path from 'path'
 import fs from 'fs'
+import deepmerge from 'deepmerge'
 
 const resolvers = fs
   .readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== 'index.js' && !file.endsWith('.test.js'))
   .map(file => path.join(__dirname, file))
-  .reduce((acc, resolverPath) => ({ ...acc, ...require(resolverPath) }), {})
+  .reduce((acc, resolverPath) => deepmerge(acc, require(resolverPath)), {})
 
 export default resolvers
